02065ef03b740ffa436158b30f9ef85a38e23e05
Dbo-Debugs <dsbowen87@gmail.com>
2020-05-08 10:00:28 -0400

    Updated Studio

---
+++ script.js
@@ -0,0 +1,12 @@
+function setSearchEngine() {
+    return document.querySelector("input[name=engine]:checked").value;
+}
+
+function init() {
+    let myform = document.getElementById("searchForm");
+    myform.addEventListener("submit", function(event) {
+        myform.action = setSearchEngine();
+    });
+}
+
+window.onload = init;
